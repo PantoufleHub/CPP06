8e2145ee93a4fdee820fbd438df00491c85edb87
Alexis Perron <aperron@c2r2s7.42lausanne.ch>
2025-02-10 14:48:26 +0100

    aeiruhf

---
+++ ScalarConverter.cpp
@@ -0,0 +1,10 @@
+#include "ScalarConverter.hpp"
+
+// ScalarConverter::ScalarConverter(){}
+
+// ScalarConverter::~ScalarConverter(){}
+
+void ScalarConverter::convert(std::string literal)
+{
+	std::cout << literal << " lol" << std::endl;
+}
